ef46a6b7dea5b9c5a3fe64d5398eb719674897b8
Celia-dlh <celiadelasheraslopez@gmail.com>
2026-02-04 18:35:33 +0100

    create S01 (1-20)

--- S02/fiboN.py
+++ S02/fiboN.py
@@ -1,1 +1,17 @@
+#ex 2
+
+def fibon(n):
+    a = 0
+    b = 1
+    for i in range(n - 1):
+        c = a + b
+        a = b  # change of variable
+        b = c
+        i = c
+    return c
+
+n5 = fibon(5)
+n10 = fibon(10)
+n15 = fibon(15)
+print(f"the 5th Fibonacci term: {n5} \nthe 10th Fibonacci term: {n10} \nthe 10th Fibonacci term: {n15}")
 
