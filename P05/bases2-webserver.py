35e9cb8dd7bdc6071b6eb08896ac56029f8b35df
Celia-dlh <celiadelasheraslopez@gmail.com>
2026-04-14 00:30:08 +0200

    create S02

--- P05/bases2-webserver.py
+++ P05/bases2-webserver.py
@@ -4,45 +4,39 @@ from pathlib import Path
 
 PORT = 8080
 bases = ["A", "C", "G", "T"]
-HTML_DIR = Path("html")
 
+socketserver.TCPServer.allow_reuse_address = True
 
-class BasesHandler(http.server.BaseHTTPRequestHandler):
+class Handler (http.server.BaseHTTPRequestHandler):
     def do_GET(self):
-        print(f"GET received! Requested path: {self.path}")
-        filepath = None
+        path = self.path.strip("/")
 
-        if self.path == "/" or self.path == "/index.html":
-            filepath = HTML_DIR / "index.html"
+        if path == "":
+            filepath = Path("html/index.html")
 
-        elif self.path.startswith("/info/"):
-            base = self.path.split("/")[-1]
+        elif path.startswith("info/"):
+            base = path.split("/")[-1]
             if base in bases:
-                filepath = HTML_DIR / "info" / f"{base}.html"
+                filepath = Path("html/info/" + base + ".html")
             else:
-                filepath = HTML_DIR / "error.html"
+                filepath = Path("html/error.html")
 
         else:
-            filename = self.path.strip("/")
-            filepath = HTML_DIR / filename
+            filepath = Path("html/" + path)
 
         try:
             content = filepath.read_bytes()
+            self.send_response(200)
         except FileNotFoundError:
-            error_path = HTML_DIR / "error.html"
-            content = error_path.read_bytes()
+            content = Path("html/error.html").read_bytes()
+            self.send_response(404)
 
-        self.send_response(200)
         self.send_header("Content-Type", "text/html")
         self.send_header("Content-Length", len(content))
         self.end_headers()
         self.wfile.write(content)
 
 # Main server
-
-Handler = BasesHandler
-socketserver.TCPServer.allow_reuse_address = True
-
 with socketserver.TCPServer(("", PORT), Handler) as httpd:
     print(f"Server running at http://localhost:{PORT}")
     try:
